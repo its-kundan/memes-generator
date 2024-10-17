9e2a6a040782b6f00daa0ee7f0a39a85f8267c4a
Kundan <kundan51kk@gmail.com>
2024-10-17 13:36:50 +0530

    error fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { reportWebVitals } from 'next/dist/build/templates/pages.';
+// import { reportWebVitals } from 'next/dist/build/templates/pages.';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Browser
     <App />
   </StrictMode>,
 )
